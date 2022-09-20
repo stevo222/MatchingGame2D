8c1de72bc83b2b6696da376ef4df33f3da5645a0
Stephen <stephenostler@yahoo.com>
2022-09-20 10:48:31 -0600

    Modify Corotuine

--- Assets/Scripts/CouratineBehavior.cs
+++ Assets/Scripts/CouratineBehavior.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
@@ -6,32 +7,59 @@ public class CouratineBehavior : MonoBehaviour
 {
 
 
-    public UnityEvent startEvent, repeatEvent, endEvent;
-    
+    public UnityEvent startEvent, startCountEvent, repeatCountEvent, endCountEvent, repeatUntilFalse;
+    public bool canRun;
     public IntData counterNum;
     public float seconds = 3.8f;
     private WaitForSeconds WfsObj;
     private WaitForFixedUpdate WffuObj;
+
+    private void Start()
+    {
+        startEvent.Invoke();
+    }
+
+    public void StartCounting()
+    {
+        StartCoroutine(Counting());
+    }
+    
     // Start is called before the first frame update
-    IEnumerator Start()
+    private IEnumerator Counting()
     {
         WfsObj = new WaitForSeconds(seconds);
         WffuObj = new WaitForFixedUpdate();
-        startEvent.Invoke();
+        startCountEvent.Invoke();
         yield return WfsObj;
 
         while (counterNum.value > 0)
         {
             //Debug.Log(counterNum);
             
-            repeatEvent.Invoke();
+            repeatCountEvent.Invoke();
             counterNum.value--;
             yield return WfsObj;
             
         }
-        endEvent.Invoke();
+        endCountEvent.Invoke();
+        
         
     }
+
+    public void StartRepeatUntilFalse()
+    {
+        canRun = true;
+        StartCoroutine(RepeatuntilFalse());
+    }
+    private IEnumerator RepeatuntilFalse()
+    {
+        while (canRun)
+        {
+            yield return WfsObj;
+            repeatUntilFalse.Invoke();
+        }
+    }
+    
 
   
 }
